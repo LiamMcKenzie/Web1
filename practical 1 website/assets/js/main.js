02d0ed55ea0b134fcad2eef8f091db455dd83106
LiamMcKenzie <mckeld3@student.op.ac.nz>
2022-05-06 11:36:27 +1200

    added to practical website

--- practical 1 website/assets/js/main.js
+++ practical 1 website/assets/js/main.js
@@ -1,2 +1,30 @@
-let messsage = "Hello!";
-console.log(messsage);
+let calendar = document.querySelector(".calendar");
+
+document.querySelector(".llegada").addEventListener("click", e => {
+    calendar.classList.toggle("active");
+})
+
+let month = document.createElement("div");
+
+let today = new Date();
+const month_name = today.toLocaleDateString('default', { month: 'long' });
+month.innerHTML = month_name;
+
+let day = today.getDate();
+console.log(day);
+
+let inner_grid = document.createElement("div");
+inner_grid.classList.add("inner_grid");
+
+for(let i = 1; i <= 31; i++)
+{
+    let square = document.createElement("div");
+    square.classList.add("square");
+    if(i < day)
+        square.classList.add("disabled");
+
+    square.innerHTML = i;
+    inner_grid.append(square);
+}
+
+calendar.append(inner_grid);
