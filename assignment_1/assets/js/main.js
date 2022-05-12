9de467d7ca4ca2d6ab567027cda37c78e1975b47
LiamMcKenzie <mckeld3@student.op.ac.nz>
2022-05-13 11:52:30 +1200

    added more to javascript

--- assignment_1/assets/js/main.js
+++ assignment_1/assets/js/main.js
@@ -1,15 +1,72 @@
-document.addEventListener("click", e => {
-    console.log(e.target);      
+const BASE_URL = "https://api.unsplash.com/";
+fetch(`${BASE_URL}/search/photos?client_id=jfisuiMFSIiyuAKdw60iD0nIP6hQshWcfN3MROGivv8&query=tea-leaves&color=white`)
+.then(response => response.json())
+.then(d => {
+  d["results"].forEach(r => {
+    background[1].style.backgroundImage = `url(${r["urls"]["full"]})`;
+
+    /*
+    let product = document.createElement("div");
+    product.classList.add("product");
+    product.style.backgroundImage = `url(${r["urls"]["regular"]})`;
+    
+    let product_title = document.createElement("div");
+    product_title.innerHTML = r["alt_description"]
+    product_title.classList.add("product_title");
+    product.append(product_title);
+    
+    document.querySelector(".product_grid").append(product);*/
+  });
 });
 
-let test = document.querySelector(".collection_flex");
 
-test.addEventListener("mouseenter", function( event ) {   
-    // highlight the mouseenter target
-    event.target.style.color = "purple";
+let collection = document.querySelectorAll(".collection_flex");
+
+collection.forEach(c => {
   
-    // reset the color after a short delay
-    setTimeout(function() {
-      event.target.style.color = "";
-    }, 500);
-  }, false);
+
+});
+
+/*
+let collection0 = document.querySelector("[data-index='0']");
+let collection1 = document.querySelector("[data-index='1']");
+let collection2 = document.querySelector("[data-index='2']");*/
+let background = document.querySelectorAll(".background_slides");
+let content = document.querySelectorAll(".sale_content");
+/*
+collection0.addEventListener("mouseenter", function( event ) {  
+  console.log("enter");
+  collection0.classList.add("active");
+  background[0].classList.add("active");
+  background[1].classList.remove("active");
+  background[2].classList.remove("active");
+}, false);
+
+collection1.addEventListener("mouseenter", function( event ) {  
+  console.log("enter");
+  collection1.classList.add("active");
+  background[1].classList.add("active");
+  background[0].classList.remove("active");
+  background[2].classList.remove("active");
+}, false);
+
+collection2.addEventListener("mouseenter", function( event ) {  
+  console.log("enter");
+  collection2.classList.add("active");
+  background[2].classList.add("active");
+  background[1].classList.remove("active");
+  background[0].classList.remove("active");
+}, false);
+
+collection0.addEventListener("mouseleave", function( event ){
+  collection0.classList.remove("active");
+}, false);
+
+collection1.addEventListener("mouseleave", function( event ){
+  collection1.classList.remove("active");
+}, false);
+
+collection2.addEventListener("mouseleave", function( event ){
+  collection2.classList.remove("active");
+}, false);*/
+
