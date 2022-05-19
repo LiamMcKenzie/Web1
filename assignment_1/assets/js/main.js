c2238f263eabb4f693c3551fa76df9715a9b7129
LiamMcKenzie <mckeld3@student.op.ac.nz>
2022-05-20 11:49:35 +1200

    added javascript to website

--- assignment_1/assets/js/main.js
+++ assignment_1/assets/js/main.js
@@ -1,40 +1,85 @@
-const BASE_URL = "https://api.unsplash.com/";
-fetch(`${BASE_URL}/search/photos?client_id=jfisuiMFSIiyuAKdw60iD0nIP6hQshWcfN3MROGivv8&query=tea-leaves&color=white`)
+/*let collection = document.querySelectorAll(".collection_flex");*/
+
+/*let collection = document.getElementById("sale_collection");*/
+
+
+const BASE_URL = "https://api.unsplash.com";
+fetch(`${BASE_URL}/search/photos?client_id=jfisuiMFSIiyuAKdw60iD0nIP6hQshWcfN3MROGivv8&query=tea-leaves`)
 .then(response => response.json())
-.then(d => {
-  d["results"].forEach(r => {
-    background[1].style.backgroundImage = `url(${r["urls"]["full"]})`;
+.then(data => {
+    let image = data["results"];
+
+    for(i=0;i<3;i++){
+      backgrounds[i].style.backgroundImage = `url(${image[i]["urls"]["full"]})`;
+    }
+
+    
+    
 
     /*
     let product = document.createElement("div");
     product.classList.add("product");
-    product.style.backgroundImage = `url(${r["urls"]["regular"]})`;
+    product.style.backgroundImage = `url(${results["urls"]["regular"]})`;
     
     let product_title = document.createElement("div");
-    product_title.innerHTML = r["alt_description"]
+    product_title.innerHTML = results["alt_description"]
     product_title.classList.add("product_title");
     product.append(product_title);
     
     document.querySelector(".product_grid").append(product);*/
-  });
+    console.log(image);
+    
 });
 
 
-let collection = document.querySelectorAll(".collection_flex");
+let collections = document.querySelectorAll(".collection_flex");
+let backgrounds = document.querySelectorAll(".background_slides");
+let content = document.querySelectorAll(".sale_content");
 
+collections.forEach((c,i) => {
+  c.addEventListener("mouseenter", function( event ) {  
+    collections.forEach(c =>{
+      c.classList.remove("active");
+    })
+
+    c.classList.add("active");
+    console.log(c);
+
+    backgrounds.forEach(b =>{
+      b.classList.remove("active");
+    })
+
+    backgrounds[i].classList.add("active");
+
+  }, false);
+
+  c.addEventListener("mouseleave", function( event ){
+    c.classList.remove("active");
+  }, false);
+})
+
+
+
+
+/*
 collection.forEach(c => {
-  
 
-});
+  collection["data-index"].addEventListener("mouseenter", function( event ) {  
+    console.log("enter " + data-index);
+    
+    
+  }, false);
+});*/
 
 /*
 let collection0 = document.querySelector("[data-index='0']");
 let collection1 = document.querySelector("[data-index='1']");
 let collection2 = document.querySelector("[data-index='2']");*/
-let background = document.querySelectorAll(".background_slides");
-let content = document.querySelectorAll(".sale_content");
+
+
 /*
 collection0.addEventListener("mouseenter", function( event ) {  
+  
   console.log("enter");
   collection0.classList.add("active");
   background[0].classList.add("active");
@@ -56,17 +101,6 @@ collection2.addEventListener("mouseenter", function( event ) {
   background[2].classList.add("active");
   background[1].classList.remove("active");
   background[0].classList.remove("active");
-}, false);
-
-collection0.addEventListener("mouseleave", function( event ){
-  collection0.classList.remove("active");
-}, false);
-
-collection1.addEventListener("mouseleave", function( event ){
-  collection1.classList.remove("active");
-}, false);
-
-collection2.addEventListener("mouseleave", function( event ){
-  collection2.classList.remove("active");
 }, false);*/
+
 
